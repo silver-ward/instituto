b48c797d15266d1610731207b2a73a00d5d0183d
null <null>
2026-06-06 15:05:15 -0300

    add conversores exercise

---
+++ conversores/app.js
@@ -0,0 +1,7 @@
+const converter = require("./conversores.js");
+
+console.log("\ncelsiusParaFahrenheit: " + converter.celsiusParaFahrenheit(30));
+console.log("\nhorasParaMinutos: " + converter.horasParaMinutos(2));
+console.log("\nidadeEmDias: " + converter.idadeEmDias(20));
+console.log("\nkmParaMetros: " + converter.kmParaMetros(3));
+console.log("\nconsumoMedio: " + converter.consumoMedio(500, 40));
---
+++ conversores/conversores.js
@@ -0,0 +1,27 @@
+const celsiusParaFahrenheit = (celsius) =>{
+return ((9/5) * celsius + 32);
+}
+
+const horasParaMinutos = (horas) => {
+    return (horas * 60);
+}
+
+const idadeEmDias = (idade) =>{
+    return (idade * 365);
+}
+
+const kmParaMetros = (km) =>{
+    return (km * 1000);
+}
+
+const consumoMedio = (distancia, litros) =>{
+    return (distancia / litros);
+}
+
+module.exports = {
+    celsiusParaFahrenheit,
+    horasParaMinutos,
+    idadeEmDias,
+    kmParaMetros,
+    consumoMedio
+}
