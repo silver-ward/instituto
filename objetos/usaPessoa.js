70db55a8cfde80223c3e5537b6ac74c5da8d1a42
silver-ward <eduard_ribeiro@hotmail.com>
2026-06-11 21:05:39 -0300

    refactor IMC presenter and change formatting

--- objetos/usaPessoa.js
+++ objetos/usaPessoa.js
@@ -1,16 +1,16 @@
 const Pessoa = require('./pessoa.js');
 
 const joao = new Pessoa("João", 80, 1.75);
-console.log(joao.nome + " IMC: " + joao.imc());
+console.log(`----------\nNome: ${joao.nome}\nIMC: ${joao.imc()}\n----------`);
 
 const pedro = new Pessoa();
 pedro.nome = "Pedro";
 pedro.peso = 100;
 pedro.altura = 1.6;
-console.log(pedro.nome + " IMC: " + pedro.imc());
+console.log(`Nome: ${pedro.nome}\nIMC: ${pedro.imc()}\n----------`);
 
 const maria = new Pessoa();
 maria.nome = "Maria";
 maria.peso = 67;
 maria.altura = 1.65;
-console.log(maria.nome + " IMC: " + maria.imc());
+console.log(`Nome: ${maria.nome}\nIMC: ${maria.imc()}\n----------`);
