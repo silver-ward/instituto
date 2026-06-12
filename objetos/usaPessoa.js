3605a333ce568bb190da28fab4a7f5e406bf6d8d
silver-ward <eduard_ribeiro@hotmail.com>
2026-06-11 21:06:59 -0300

    add IMC rounded print value

--- objetos/usaPessoa.js
+++ objetos/usaPessoa.js
@@ -1,16 +1,16 @@
 const Pessoa = require('./pessoa.js');
 
 const joao = new Pessoa("João", 80, 1.75);
-console.log(`----------\nNome: ${joao.nome}\nIMC: ${joao.imc()}\n----------`);
+console.log(`----------\nNome: ${joao.nome}\nIMC: ${joao.imc().toFixed(2)}\n----------`);
 
 const pedro = new Pessoa();
 pedro.nome = "Pedro";
 pedro.peso = 100;
 pedro.altura = 1.6;
-console.log(`Nome: ${pedro.nome}\nIMC: ${pedro.imc()}\n----------`);
+console.log(`Nome: ${pedro.nome}\nIMC: ${pedro.imc().toFixed(2)}\n----------`);
 
 const maria = new Pessoa();
 maria.nome = "Maria";
 maria.peso = 67;
 maria.altura = 1.65;
-console.log(`Nome: ${maria.nome}\nIMC: ${maria.imc()}\n----------`);
+console.log(`Nome: ${maria.nome}\nIMC: ${maria.imc().toFixed(2)}\n----------`);
