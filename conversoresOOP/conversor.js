dffb11bc3ade5f154e6598c397779086ecdab496
null <null>
2026-06-16 22:57:45 -0300

    add kmParaMilhas function

--- conversoresOOP/conversor.js
+++ conversoresOOP/conversor.js
@@ -1,6 +1,10 @@
 class Conversor{
     celsiusParaFahrenheit(celsius){
-        return ((9/5) * celsius + 32)
+        return ((9/5) * celsius + 32);
+    }
+
+    kmParaMilhas(km){
+        return (km * 0.621371);
     }
 }
 
--- conversoresOOP/main.js
+++ conversoresOOP/main.js
@@ -2,4 +2,5 @@ const Conversor = require("./conversor.js");
 
 const Converter = new Conversor();
 
-console.log(`25ºC = ${Converter.celsiusParaFahrenheit(25)}ºF`);
+console.log(`25ºC = ${Converter.celsiusParaFahrenheit(25)}ºF.`);
+console.log(`10km = ${Converter.kmParaMilhas(10)} milhas.`);
