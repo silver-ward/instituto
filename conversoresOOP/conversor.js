2cbd348f9e0cf779454fb35f632f7759d63361a4
null <null>
2026-06-16 22:43:49 -0300

    add oop conversor with celsiusParaFahrenheit function

---
+++ conversoresOOP/conversor.js
@@ -0,0 +1,7 @@
+class Conversor{
+    celsiusParaFahrenheit(celsius){
+        return ((9/5) * celsius + 32)
+    }
+}
+
+module.exports = Conversor;
---
+++ conversoresOOP/main.js
@@ -0,0 +1,5 @@
+const Conversor = require("./conversor.js");
+
+const Converter = new Conversor();
+
+console.log(`25ºC = ${Converter.celsiusParaFahrenheit(25)}ºF`);
