60a2a13527dddb9e84be757065bde4d4ff06df7c
null <null>
2026-06-18 20:35:27 -0300

    add Pessoa Fisica subclass

---
+++ objetos/pessoas/PF.js
@@ -0,0 +1,18 @@
+const Pessoa = require("./Pessoa.js")
+
+class PF extends Pessoa{
+    #cpf
+
+    setCPF(cpf){
+        if(cpf){
+            this.#cpf = cpf;
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+    getCPF(){
+        return this.#cpf;
+    }
+}
