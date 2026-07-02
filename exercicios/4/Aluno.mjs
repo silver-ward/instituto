229d12d87ebbb90e9f02285b2302e133f02701cd
silver-ward <eduard_ribeiro@hotmail.com>
2026-07-02 20:13:25 -0300

    add all Aluno methods

--- exercicios/4/Aluno.mjs
+++ exercicios/4/Aluno.mjs
@@ -1,11 +1,17 @@
+import { Pessoa } from "./Pessoa.mjs";
+
 class Aluno extends Pessoa{
     #matricula;
 
     setMatricula(matricula){
+        if(matricula){
+            #this.matricula = matricula;
+            return true;
+        }
         return false;
     }
 
     getMatricula(){
-
+        return this.#matricula;
     }
 }
