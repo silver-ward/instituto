78826a26d499d21080ef170d110480beeb21e546
silver-ward <eduard_ribeiro@hotmail.com>
2026-07-02 20:34:35 -0300

    add Teste sectioning and Teste 2

--- exercicios/4/usaPessoas.mjs
+++ exercicios/4/usaPessoas.mjs
@@ -1,5 +1,7 @@
 import { Pessoa } from "./Pessoa.mjs";
 
+console.log('\nTeste 1:')
+
 const p = new Pessoa();
 
 p.setNome('João Silva');
@@ -7,3 +9,23 @@ p.setEmail('joao@email.com');
 
 console.log(p.getNome());
 console.log(p.getEmail());
+
+console.log('\nTeste 2:')
+
+const p2 = new Pessoa();
+
+if (p.setNome('')) {
+    console.log('Operação realizada com sucesso.');
+}
+else {
+    console.log('Nome inválido');
+}
+
+if (p.setEmail('')) {
+    console.log('Operação realizada com sucesso.');
+}
+else {
+    console.log('Email inválido');
+}
+
+
