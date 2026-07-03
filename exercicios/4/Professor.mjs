516411e6844473bb9b0759139e54ec39b194b9c9
silver-ward <eduard_ribeiro@hotmail.com>
2026-07-02 21:45:34 -0300

    fix setDisciplina method and export class

--- exercicios/4/Professor.mjs
+++ exercicios/4/Professor.mjs
@@ -1,11 +1,11 @@
 import { Pessoa } from "./Pessoa.mjs";
 
-class Professor extends Pessoa {
+export default class Professor extends Pessoa {
     #disciplina
 
     setDisciplina(disciplina) {
-        if (disciplia) {
-            this.#disciplina;
+        if (disciplina) {
+            this.#disciplina = disciplina;
             return true;
         }
         return false;
