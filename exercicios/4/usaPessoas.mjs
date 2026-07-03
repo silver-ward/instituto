64c4c685035860b63945c8b397c454cf856cb471
silver-ward <eduard_ribeiro@hotmail.com>
2026-07-02 21:55:26 -0300

    add Teste 6

--- exercicios/4/usaPessoas.mjs
+++ exercicios/4/usaPessoas.mjs
@@ -2,7 +2,7 @@ import { Pessoa } from "./Pessoa.mjs";
 import { Aluno } from "./Aluno.mjs";
 import Professor from "./Professor.mjs";
 
-console.log('\nTeste 1:')
+console.log('\nTeste 1:');
 
 const p = new Pessoa();
 
@@ -12,7 +12,7 @@ p.setEmail('joao@email.com');
 console.log(p.getNome());
 console.log(p.getEmail());
 
-console.log('\nTeste 2:')
+console.log('\nTeste 2:');
 
 const p2 = new Pessoa();
 
@@ -30,7 +30,7 @@ else {
     console.log('Email inválido');
 }
 
-console.log('\nTeste 3:')
+console.log('\nTeste 3:');
 
 const a = new Aluno();
 
@@ -42,7 +42,7 @@ console.log(a.getNome());
 console.log(a.getEmail());
 console.log(a.getMatricula());
 
-console.log('\nTeste 4:')
+console.log('\nTeste 4:');
 
 const b = new Aluno();
 
@@ -67,7 +67,7 @@ else {
     console.log('Matrícula inválida');
 }
 
-console.log('\nTeste 5:')
+console.log('\nTeste 5:');
 
 const prof = new Professor();
 
@@ -78,3 +78,28 @@ prof.setDisciplina('Programação Web');
 console.log(prof.getNome());
 console.log(prof.getEmail());
 console.log(prof.getDisciplina());
+
+console.log('\nTeste 6:');
+
+const c = new Professor();
+
+if (c.setNome('Ana')) {
+    console.log('Nome cadastrado com sucesso.');
+}
+else {
+    console.log('Nome inválido');
+}
+
+if (c.setEmail('ana@email.com')) {
+    console.log('Email cadastrado com sucesso.');
+}
+else {
+    console.log('Email inválido');
+}
+
+if (c.setDisciplina('')) {
+    console.log('Disciplina cadastrada com sucesso.');
+}
+else {
+    console.log('Disciplina inválida');
+}
