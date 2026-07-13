6d7fe77686ad3bd858f7a98b8ef59b8b519bb391
silver-ward <eduard_ribeiro@hotmail.com>
2026-07-13 14:57:07 -0300

    add PF methods

--- exercicios/5/pessoas/PF.mjs
+++ exercicios/5/pessoas/PF.mjs
@@ -4,6 +4,10 @@ export default class PF extends Pessoa{
     #cpf;
 
     setCPF(cpf){
+        if(cpf.length >= 14){
+            this.#cpf = cpf;
+            return true;
+        }
         return false;
     }
 
