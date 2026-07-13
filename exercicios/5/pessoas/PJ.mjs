eda2082125cc1b2726b7c01e451fbf3a643b8a24
silver-ward <eduard_ribeiro@hotmail.com>
2026-07-13 16:00:15 -0300

    add PJ methods

---
+++ exercicios/5/pessoas/PJ.mjs
@@ -0,0 +1,24 @@
+import Pessoa from './Pessoa.mjs';
+
+export default class PJ extends Pessoa{
+    #cnpj;
+
+    setEmail(email){
+        if (email.includes('@')){
+            return super.setEmail(email);
+        }
+        return false;
+    }
+
+    setCNPJ(cnpj){
+        if(cnpj.length >= 18){
+            this.#cnpj = cnpj;
+            return true;
+        }
+        return false;
+    }
+
+    getCNPJ(){
+        return this.#cnpj;
+    }
+}
