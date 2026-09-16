c0f47952a69ae01119254fa31ffd749444ecb7f6
silver-ward <eduard_ribeiro@hotmail.com>
2026-09-16 20:56:01 -0300

    add inputpassword state

--- reactJS/2/src/App.jsx
+++ reactJS/2/src/App.jsx
@@ -1,4 +1,5 @@
 import InputText from "./components/states/InputText.jsx";
+import InputPassword from "./components/states/InputPassword.jsx";
 import Label from "./components/states/Label.jsx";
 import Labelln from "./components/states/Labelln.jsx";
 
@@ -8,8 +9,9 @@ function App() {
     <>
       <Labelln texto="Tela de Autenticação" />
       <Label texto="Usuário: "/>
-      <InputText placeholder="Digite o usuário" /> <br />
+      <InputText placeholder="Digite o usuário" /><br />
       <Label texto="Senha: "/>
+      <InputPassword placeholder="Digite a senha"/>
     </>
   );
 }
