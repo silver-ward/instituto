c3d730f7bcddff9495bb3e7940af5ab177c6ac56
silver-ward <eduard_ribeiro@hotmail.com>
2026-09-16 20:56:34 -0300

    add button state

--- reactJS/2/src/App.jsx
+++ reactJS/2/src/App.jsx
@@ -2,6 +2,7 @@ import InputText from "./components/states/InputText.jsx";
 import InputPassword from "./components/states/InputPassword.jsx";
 import Label from "./components/states/Label.jsx";
 import Labelln from "./components/states/Labelln.jsx";
+import Button from "./components/states/Button.jsx";
 
 
 function App() {
@@ -11,7 +12,8 @@ function App() {
       <Label texto="Usuário: "/>
       <InputText placeholder="Digite o usuário" /><br />
       <Label texto="Senha: "/>
-      <InputPassword placeholder="Digite a senha"/>
+      <InputPassword placeholder="Digite a senha"/><br />
+      <Button valor="Login"/>
     </>
   );
 }
---
+++ reactJS/2/src/components/states/Button.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+export default function Button({ valor }) {
+
+  const [texto, setTexto] = useState(valor);
+
+  function handleClick() {
+    setTexto("Você clicou!");
+  }
+
+  return (
+    <button onClick={handleClick}>
+      {texto}
+    </button>
+  );
+}
