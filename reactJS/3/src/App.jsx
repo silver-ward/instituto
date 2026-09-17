fbc1049aa9c1a9c4783da07a6e09e86b38b49f70
silver-ward <eduard_ribeiro@hotmail.com>
2026-09-17 20:21:32 -0300

    add InputPassword.jsx + css

--- reactJS/3/src/App.jsx
+++ reactJS/3/src/App.jsx
@@ -1,6 +1,7 @@
 import LabelTitle from "./components/css/LabelTitle.jsx";
 import Label from "./components/css/Label.jsx";
 import InputText from "./components/css/InputText.jsx";
+import InputPassword from "./components/css/InputPassword.jsx";
 
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
       <Label texto="Usuário: "/>
       <InputText placeholder="Digite o nome de usuário aqui."/>
       <Label texto="Senha: " />
-      <InputText placeholder="Digite a senha aqui."/>
+      <InputPassword placeholder="Digite a senha aqui."/>
       
     </>
   );
