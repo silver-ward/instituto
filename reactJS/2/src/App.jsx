1f75ff878729c65ead9e1b395efb9c7cb3abecdd
silver-ward <eduard_ribeiro@hotmail.com>
2026-09-16 21:01:51 -0300

    refactor: condense all previous states into Login

--- reactJS/2/src/App.jsx
+++ reactJS/2/src/App.jsx
@@ -1,19 +1,10 @@
-import InputText from "./components/states/InputText.jsx";
-import InputPassword from "./components/states/InputPassword.jsx";
-import Label from "./components/states/Label.jsx";
-import Labelln from "./components/states/Labelln.jsx";
-import Button from "./components/states/Button.jsx";
+import Login from "./components/states/Login.jsx";
 
 
 function App() {
   return (
     <>
-      <Labelln texto="Tela de Autenticação" />
-      <Label texto="Usuário: "/>
-      <InputText placeholder="Digite o usuário" /><br />
-      <Label texto="Senha: "/>
-      <InputPassword placeholder="Digite a senha"/><br />
-      <Button valor="Login"/>
+      <Login />
     </>
   );
 }
